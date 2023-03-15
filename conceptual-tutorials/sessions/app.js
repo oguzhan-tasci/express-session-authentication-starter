bd26fe517155f00ae3e0a1719c7e66d58a6466eb
oyeProjects <oyeprojects@hotmail.com>
2023-03-15 06:45:33 +0300

    express sessions

--- conceptual-tutorials/sessions/app.js
+++ conceptual-tutorials/sessions/app.js
@@ -4,6 +4,13 @@ const session = require('express-session');
 
 // Package documentation - https://www.npmjs.com/package/connect-mongo
 const MongoStore = require('connect-mongo')(session);
+/*
+'session' ve 'cookie' arasındaki fark nedir?
+    Temel fark verilerin depolandığı yerdir.
+    Cookie verileri tarayıcıda saklar ve her http isteğine key-value değerini bağlar.
+    Session is sunucu tarafında(server-side) yani express'te verilerini tutar.
+    Server-side oldukça avantajlıdı çünkü ulaşması zordur bu yüzden gizli bilgileri tutabiliriz ama cookie için aynısı söylenemez.
+*/
 
 // Create the Express application
 var app = express();
@@ -13,7 +20,9 @@ const dbOptions = {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }
-const connection = mongoose.createConnection(dbString, dbOptions);
+const connection = mongoose.createConnection(dbString, dbOptions,() => {
+    console.log("Database have connected!");
+});
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -25,23 +34,36 @@ const sessionStore = new MongoStore({
 
 app.use(session({
     secret: 'some secret',
-    resave: false,
-    saveUninitialized: true,
+    resave: false, // resave ve saveUninitialized parametreleri, farklı tetiklemelerde nasıl tepki vereceğini belirttiğimiz yerdir.
+    saveUninitialized: true,  // örneğin bir değişiklik olmadı veya oldu.
     store: sessionStore,
+    // Geçen derste gördüğümüz gibi cookie'nin expire,signed,httpOnly,path,secure,sameSite gibi özellikleri vardır.
+    // Bunlar içinde biz en ilgilenlerden biri 'expire'dır.
     cookie: {
         maxAge: 1000 * 60 * 60 * 24 // Equals 1 day (1 day * 24 hr/1 day * 60 min/1 hr * 60 sec/1 min * 1000 ms / 1 sec)
     }
 }));
 
-app.get('/', (req, res, next) => {
+// Ana sayfaya istek attığımız zaman cookie bölümünde "connect.sid" ve onun value değerini görüyoruz.(connect.sid default değerdir).
+app.get("/", (req,res,next) => {
+    console.log(req.session);
+    res.send("<h1>Hello World! Take a look to the cookies!</h1>");
+})
+
+// Session'a bizlerde parametreler ekleyip neler olup bittiğinden haberdar olabiliriz.
+// About sayfasına kaç kere girildiğini hesaplamak için aşağıdaki işlemi yapıyoruz.
+// Bilgileri takip etmek için oldukça önemli olduğunu görüyoruz...
+app.get('/about', (req, res, next) => {
     
-    if (req.session.viewCount) {
-        req.session.viewCount = req.session.viewCount + 1;
+    if (req.session.oguzhanCount) {
+        req.session.oguzhanCount = req.session.oguzhanCount + 1;
     } else {
-        req.session.viewCount = 1;
+        req.session.oguzhanCount = 1;
     }
 
-    res.send(`<h1>You have visited this page ${req.session.viewCount} times.</h1>`)
+    res.send(`<h1>You have visited this page ${req.session.oguzhanCount} times.</h1>`)
 });
+
+
 
 app.listen(3000);
